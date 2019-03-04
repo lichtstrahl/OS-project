961575ffb42790f35acc7327ec189afcc6f0bfae
Igor <cool.rainbow2012@yandex.ru>
2019-03-04 14:25:03 +0300

    Добавлены специальные символы

--- c_client/const.h
+++ c_client/const.h
@@ -12,41 +12,75 @@
 #include <stdio.h>
 #define SEND_MSG(msg) printf(msg "\n")
 #define MSG_FILE_INFO_NOT_FOUND "Не удалось найти файл модуля. Вероятно он не был загружен или был отключен."
+#define MSG_NEXT_CLICK_EXIT     "(Нажать на СКМ, чтобы выйти)"
 
 #define SET_LOW_LETTERS "Маленькие буквы"
 #define SET_BIG_LETTERS "Большие буквы"
+#define SET_SYMBOLS     "Символы"
+#define SET_SPECIAL     "Специальные символы"
+
+enum SetType {
+    COMMON,
+    SPECIFIC
+};
+
+
 
 struct Set
 {
     std::string name;
-    char *set;
+    std::string *set;
     int length;
+    SetType type;
 };
 typedef struct Set Set;
 
 const Set low_letters = {
         SET_LOW_LETTERS,
-        new char[26] {
-            'a','b','c','d','e','f','g','h','i',
-            'j','k','l','m','n','o','p','q','r',
-            's','t','u','v','w','x','y','z'
+        new string[26] {
+            "a","b","c","d","e","f","g","h","i",
+            "j","k","l","m","n","o","p","q","r",
+            "s","t","u","v","w","x","y","z"
         },
-        26
+        26,
+        SetType::COMMON
 };
 
 const Set big_letters = {
         SET_BIG_LETTERS,
-        new char[26] {
-            'A','B','C','D','E','F','G','H','I',
-            'J','K','L','M','N','O','P','Q','R',
-            'S','T','U','V','W','X','Y','Z'
+        new string[26] {
+            "A","B","C","D","E","F","G","H","I",
+            "J","K","L","M","N","O","P","Q","R",
+            "S","T","U","V","W","X","Y","Z"
         },
-        26
+        26,
+        SetType::COMMON
 };
 
-#define COUNT_AVAILABLE_SET 2
+const Set symbols = {
+        SET_SYMBOLS,
+        new string[10] {
+            "-","=","+","*","\"",">","<",",",".","/"
+        },
+        10,
+        SetType::COMMON
+};
+
+
+#define SPECIAL_SPACE           "[Space]"
+#define SPECIAL_BACKSPACE       "[Backspace]"
+const Set special = {
+        SET_SPECIAL,
+        new string[2] {
+            SPECIAL_SPACE, SPECIAL_BACKSPACE
+        },
+        2,
+        SetType::SPECIFIC
+};
+
+#define COUNT_AVAILABLE_SET 4
 const Set availableSet[COUNT_AVAILABLE_SET] = {
-        low_letters, big_letters
+        low_letters, big_letters,symbols, special
 };
 
 #endif //C_CLIENT_CONST_H
--- c_client/main.cpp
+++ c_client/main.cpp
@@ -5,29 +5,34 @@
 #include <fstream>
 #include "const.h"
 #include <vector>
+#include <string>
+#include <string.h>
 
 using std::ifstream;
 using std::vector;
-
+using std::string;
 
 #define MAX_COUNT "10"
 #define DATA_PATH "/proc/mouseListener/info"
-#define VERSION "v.2.0.1"
+#define VERSION "v.2.1.0 Добавлены специальные символы [Space] [Backspace]"
 
 
 #define MSG_MAX_CONT_ACTION "Всего будет обработано " MAX_COUNT " действий"
-
 
 class Model
 {
 private:
     int currentIndex;
     int currentSet;
-    vector<char> buffer;
+    vector<string> buffer;
 
-    char getCurrentElement() {
+    string getCurrentElement() {
         return availableSet[currentSet].set[currentIndex];
     }
+    Set getCurrentSet() {
+        return availableSet[currentSet];
+    }
+
 public:
     Model()
     {
@@ -37,15 +42,15 @@ public:
 
     void print() {
         printf("Текущее множество: %s;\n", availableSet[currentSet].name.c_str());
-        printf("Текущий элемент: %c;\n", getCurrentElement());
-        printf("Буффер: %s;\n", buffer_to_str());
+        printf("Текущий элемент: %s;\n", getCurrentElement().c_str());
+        printf("Буффер: %s;\n", buffer.size() != 0 ? buffer_to_str() : MSG_NEXT_CLICK_EXIT);
     }
 
     char* buffer_to_str() {
         static char buf[1024];
         int i;
         for (i = 0; i < buffer.size(); i++)
-            buf[i] = buffer[i];
+            buf[i] = buffer[i].c_str()[0];
         buf[i] = '\0';
         return buf;
     }
@@ -53,10 +58,10 @@ public:
     bool processingAction(Action a) {
         switch (a.type) {
             case CLICK_LEFT:
-                buffer.push_back(getCurrentElement());
+                processingClickLeft();
                 break;
             case CLICK_RIGHT:
-
+                nextSet();
                 break;
 
             case CLICK_MIDDLE: {
@@ -82,6 +87,37 @@ public:
 
     }
 
+    void processingClickLeft() {
+
+        switch (getCurrentSet().type) {
+            case SetType::COMMON:
+                buffer.push_back(getCurrentElement());
+                break;
+
+            case SetType::SPECIFIC: {
+                string action = getCurrentElement();
+                if (!strcmp(action.c_str(), SPECIAL_SPACE) && buffer.size() > 0) {   // Обработка пробела
+                    buffer.push_back(" ");
+                }
+
+                if (!strcmp(action.c_str(), SPECIAL_BACKSPACE) && buffer.size() > 0) {
+                    buffer.pop_back();
+                }
+
+
+                break;
+            }
+
+        }
+    }
+
+    void nextSet() {
+        if (++currentSet == COUNT_AVAILABLE_SET) {
+            currentSet = 0;
+        }
+        currentIndex = 0;
+    }
+
     void forwardElement() {
         if (++currentIndex == availableSet[currentSet].length) currentIndex = 0;
     }
@@ -93,8 +129,7 @@ public:
 };
 
 int main(int argc, char **argv) {
-    SEND_MSG(VERSION);
-    SEND_MSG(MSG_MAX_CONT_ACTION);
+
     FileHolder fileHolder(DATA_PATH);
     Action oldAction = {-1, CLICK_LEFT, nullptr};
     Model model;
@@ -103,8 +138,8 @@ int main(int argc, char **argv) {
     while (true) {
         Action newAction = fileHolder.readFile();
         if (newAction.id != oldAction.id) {
-//            system("clear");
-            PRINT_ACTION(newAction);
+            system("clear");
+            SEND_MSG(VERSION);
             bool exit = model.processingAction(newAction);
             bufferAction.push_back(newAction);
             model.print();
@@ -117,7 +152,8 @@ int main(int argc, char **argv) {
 
         PAUSE_MS(200);
     }
-    system("bash ./stop.sh");
 
+
+    system("bash ./stop.sh");
     return 0;
 }
